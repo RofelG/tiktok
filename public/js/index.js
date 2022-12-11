c0cf6904353f2c6099543a9b1645fc871ca3b032
Rofel Ganado <rofel_p_ganado@hotmail.com>
2022-12-11 05:00:12 -0500

    Added videos in place of the coloured elements Added ability to iterate through data for videos Added ability to unmute videos by tapping Footer

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,10 @@
 let lastKnownScrollPosition = 0;
 let scrolling = false;
 let nextFeed;
+let mute = true;
 
-function addItem() {
-  if (scrolling) {
+function addItem(scroll = false) {
+  if (scrolling || scroll) {
     console.log('Stop Interval');
     clearInterval(scrollInterval);
     lastKnownScrollPosition++;
@@ -14,20 +15,22 @@ function addItem() {
 
     console.log(lastKnownScrollPosition);
 
-    let containerList = document.querySelector('.container-list');
-    let arr = ['one', 'two', 'three', 'four'];
+    // let containerList = document.querySelector('.container-list');
+    // let arr = ['one', 'two', 'three', 'four'];
     
-    for (let i = 0; i < 4; i++) {
-      let section = document.createElement('section');
-      let temp = i + 1;
-      if (temp > 4) temp = 1;
-      section.classList.add('container-list__item', arr[temp - 1]);
+    // for (let i = 0; i < 4; i++) {
+    //   let section = document.createElement('section');
+    //   let temp = i + 1;
+    //   if (temp > 4) temp = 1;
+    //   section.classList.add('container-list__item', arr[temp - 1]);
 
-      containerList.append(section);
-    }
+    //   containerList.append(section);
+    // }
 
     refreshFeed().then((data) => {
-      console.log(data);
+      console.log('FEED DATA', data);
+      console.log('Received Data');
+      appendItems(data);
     });
 
     scrolling = false;
@@ -37,29 +40,89 @@ function addItem() {
   }
 }
 
+function appendItems(data) {
+  console.log("DATA", data);
+  let containerList = document.querySelector('.container-list');
+  
+  let feed = data.data;
+  let success = data.success;
+
+  if (!success) return;
+
+  for (let i = 0; i < feed.length; i++) {
+
+    let type = feed[i].type ? feed[i].type : feed[i].images[0].type;
+
+    console.log("TYPE", type, "FEED", feed[i]);
+
+    if (!type.includes('video/mp4') && !type.includes('image/gif')) {
+      console.log("NOT A VIDEO");
+      continue;
+    }
+
+    console.log("Added to FEED");
+
+    let section = document.createElement('section');
+    section.classList.add('container-list__item');
+    section.setAttribute('data-id', feed[i].id);
+
+    let video = document.createElement('video');
+    video.classList.add('container-list__item--video');
+    video.setAttribute('loop', 'loop');
+    video.setAttribute('playsinline', 'playsinline');
+    video.setAttribute('poster', (feed[i].gifv ? feed[i].mp4 : feed[i].images[0].gifv));
+
+    let source = document.createElement('source');
+    source.setAttribute('type', 'video/mp4');
+
+    source.setAttribute('src', (feed[i].mp4 ? feed[i].mp4 : feed[i].images[0].mp4));
+
+    video.append(source);
+    section.append(video);
+
+    containerList.append(section);
+
+    observer.observe(section);
+
+  }
+}
+
 async function refreshFeed() {
 
   var myHeaders = new Headers();
-  // myHeaders.append("Authorization", "Bearer " + accessToken);
-  myHeaders.append("Authorization", "Client-ID " + clientId);
+  myHeaders.append("Authorization", "Bearer " + accessToken);
+  // myHeaders.append("Authorization", "Client-ID " + clientId);
   myHeaders.append("Accept", "application/json");
 
   var requestOptions = {
     method: 'GET',
-    headers: myHeaders,
-    redirect: 'follow'
+    headers: myHeaders
   };
+  
+  const response = await fetch("https://api.imgur.com/3/gallery/random/", requestOptions);
+  // const response = await fetch("/static/js/viral.json", requestOptions);
 
-  fetch("https://api.imgur.com/3/feed/hot/viral", requestOptions)
-    .then(response => response.text())
-    .then(result => {
-      console.log(result);
-      return result;
-    })
-    .catch(error => console.log('error', error));
+  return response.json();
 }
 
+const intersectOptions = {
+  rootMargin: '0px',
+  threshold: 0.2
+}
 
+const intersectCallback = (entries, observer) => {
+  entries.forEach(entry => {
+    let video = entry.target.querySelector('video');
+    if (entry.isIntersecting) {
+      video.muted = mute;
+      video.play();
+    } else {
+      video.pause();
+    }
+  });
+};
+
+const observer = new IntersectionObserver(intersectCallback, intersectOptions);
 
 let containerFluidList = document.querySelector('.container-fluid--list');
 
@@ -70,3 +133,11 @@ containerFluidList.addEventListener('scroll', (e) => {
 });
 
 let scrollInterval = setInterval(addItem, 1000);
+
+addItem(true);
+
+let footer = document.querySelector('footer');
+
+footer.addEventListener('click', (e) => {
+  mute = false;
+});
